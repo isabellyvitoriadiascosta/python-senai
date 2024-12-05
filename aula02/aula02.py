68de7841020c296209f6b57a6ba8e1b8e767131b
isabellyvitoriadiascosta <isabellycostaif391886@gmail.com>
2024-12-05 11:33:21 -0300

    Add files via upload

---
+++ aula02/aula02.py
@@ -0,0 +1,9 @@
+ print  ("good morning !!") #tipo -> str=string
+ idade_usuario=21 #tipo->float= flutuante
+ altura_usuario=1.77 tipo -> bool= boolean
+
+
+
+
+Print('sua idade e,idade_usuario,'18)
+print('você tem")
